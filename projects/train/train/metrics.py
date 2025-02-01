cd65426fc8cea35a8a624cd904a355ef1a099be0
Chia-Jui Chou <agoodmanjerry@gmail.com>
2025-02-01 05:52:37 -0600

    Fix the typos in projects/train/metrics.py

--- projects/train/train/metrics.py
+++ projects/train/train/metrics.py
@@ -132,7 +132,6 @@ class OnlinePsdRatio(Metric):
         # postprocess, doing the bandpass filtering back
         # in numpy because torchaudio won't work
         noise = torch.stack(noise).double()
-        noise_before_process = noise
         noise = self.y_scaler(noise, reverse=True)
         noise = self.bandpass(noise.cpu().detach().numpy())
         noise = torch.tensor(noise, device=device)
@@ -146,7 +145,7 @@ class OnlinePsdRatio(Metric):
         # reshape our raw strain into a timeseries
         raw = torch.cat(self.strain, dim=0)[1 : num_frames - 1]
         raw = raw.view(1, -1)
-        return noise_before_process, noise, raw.double()
+        return noise, raw.double()
 
     def compute(self, reduce: bool = True):
         noise, raw = self.clean()
