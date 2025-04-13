6987611c79f2b76237363dc0985ae38fc1edd09a
Muhammed Saleem <muhammed.saleem@ligo.org>
2025-04-13 10:00:19 -0700

    more changes

--- deepclean/couplings/__init__.py
+++ deepclean/couplings/__init__.py
@@ -2,4 +2,5 @@ from .coupling import Coupling, SubtractionProblem, subtraction_problems
 from .sub_60Hz import Sub60HzRestricted
 from .sub_60Hz_origin import Sub60Hz
 from .sub_60Hz_O3_MDC import Sub60Hz_O3_MDC
+from .sub_CDC_120Hz import SubO4CDC120Hz
 # from .sub_104Hz import Sub104Hz
