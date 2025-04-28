420c2e057b90cd51d64fd5a274b2bdce0205ad5e
Christina Reissel <christina.reissel@cern.ch>
2025-04-28 08:01:32 -0700

    first functioning offline inference

--- deepclean/tasks/infer.py
+++ deepclean/tasks/infer.py
@@ -1,29 +1,53 @@
-import law
-import luigi
 import os
 
-from deepclean.base import DeepCleanTask
+import law
+import luigi
 
-class Clean(DeepCleanTask):
+from deepclean.base import DeepCleanTask
+from deepclean.tasks.condor.workflows import StaticMemoryWorkflow
+
+class Clean(DeepCleanTask, law.LocalWorkflow, StaticMemoryWorkflow):
     train_config = luigi.Parameter(
         default="/opt/deepclean/projects/train/config.yaml"
     )
     input_dir = luigi.Parameter()
     output_dir = luigi.Parameter()
+    train_dir = luigi.Parameter()
 
+    @property
+    def python(self):
+        return "/opt/env/bin/python"
+
+    @law.dynamic_workflow_condition
+    def workflow_condition(self) -> bool:
+        return True
+
+    @workflow_condition.create_branch_map
     def create_branch_map(self):
         filenames = [f for f in os.listdir(self.input_dir) if '.hdf5' in f]
-        files = [self.input_dir for f in filenames]
+        files = [self.input_dir+f for f in filenames]
         branch_map = dict(enumerate(files))
         return branch_map
 
+    @workflow_condition.output
     def output(self):
         # TODO: needs proper naming to not overwrite files
         fname = f"output_test.hdf5"
         return law.LocalFileTarget(fname)
 
+    def get_file_info(self):
+        filename = self.branch_data
+        f = filename.split('/')[-1]
+        duration = f.split('-')[-1].replace('.hdf5', '')
+        return float(duration)
+
     @property
     def command(self) -> list[str]:
+
+        duration = self.get_file_info()
+        train_duration = duration/2.0
+        test_duration = duration/2.0
+
         channels = [self.strain_channel] + self.witnesses
         command = [
             self.python,
@@ -38,5 +62,12 @@ class Clean(DeepCleanTask):
             str(self.cfg.freq_low),
             "--data.freq_high",
             str(self.cfg.freq_high),
+            "--trainer.logger.save_dir",
+            self.train_dir,
+            "--data.train_duration",
+            str(train_duration),
+            "--data.test_duration",
+            str(test_duration),
         ]
+
         return command
--- projects/infer/infer/cli.py
+++ projects/infer/infer/cli.py
@@ -8,8 +8,11 @@ from train.data import DeepCleanDataset
 from train.model import DeepClean
 from utils.logging import configure_logging
 from train.cli import AframeCLI
+from jsonargparse import ActionConfigFile, ArgumentParser
+
 
 def main(args=None):
+
     cli = AframeCLI(
         model_class=DeepClean,
         datamodule_class=DeepCleanDataset,
@@ -20,11 +23,25 @@ def main(args=None):
         args=args,
     )
 
-    weights = torch.load(cli.trainer.checkpoint_callback.best_model_path)
-    cli.model.load_state_dict(weights["state_dict"])
-    cli.trainer.test(cli.model, cli.datamodule)
+    # TODO: we need to update the dataset such that it can be loaded in completely, see here: https://github.com/ML4GW/ml4gw/blob/main/ml4gw/dataloading/hdf5_dataset.py 
 
+    # recover model from checkpoint
+    # TODO: read model checkpoint from config or similar
+    weights = torch.load("/home/christina.reissel/deepclean/results/test-infer/lightning_logs/version_0/checkpoints/68-2139.ckpt")
+    cli.model.load_state_dict(weights["state_dict"])
+    # initilize scaler
+    for i in ["X", "y"]:
+        name = f"{i}_scaler"
+        module = getattr(cli.datamodule, name)
+        values = torch.load(os.path.join("/home/christina.reissel/deepclean/results/test-infer/lightning_logs/version_0/", f"{name}.pt"))
+        module.load_state_dict(values)
+
+    # run predictions
+    cli.trainer.predict(cli.model, datamodule=cli.datamodule)
+    # combine predictions to one strain
+    noise, raw = cli.model.metric.compute(reduce=False)
+
+    # TODO: save noise and raw output in new frame file
 
 if __name__ == "__main__":
     main()
-
